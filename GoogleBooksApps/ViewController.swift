3cf5dbc42c5cfd0c15763c328415e3f08ddd8ed2
C2QJG01SDRJD <C2QJG01SDRJD@ACA814FB.ipt.aol.com>
2019-03-18 14:18:07 -0400

    Test Changes

--- GoogleBooksApps/ViewController.swift
+++ GoogleBooksApps/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     let service = NetworkService()
     let realmService = RealmService()
     let disposeBag = DisposeBag()
+    let intArr = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
